728403dc08fe99a200d47961d14a64509e919f02
Siddharth Jan Simon <65785966+Siddysimon@users.noreply.github.com>
2023-09-20 00:51:37 +0000

    Fix: Working model framework Test: Start to Scene

--- model/model.py
+++ model/model.py
@@ -5,20 +5,16 @@ import json
 class Node:
     """Node class for the decision tree"""
 
-    def __init__(self, node_id):
+    def __init__(self, node_id, model_data):
         self.node_id = node_id
-        self.data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir, "/data/data.json")) #TODO: change this to the correct path
-        print(self.data_path)
         self.thresholds = []
-        node_id = ""
         self.next_nodes = []
-        self.load_data()
+        self.load_node(model_data)
 
-    # loads node data from json file
-    def load_data(self):
+    # loads node data from passed model data from Patient
+    def load_node(self, model_data):
         """loads node data from json file"""
-        node_data = json.loads(self.data_path).get("Nodes", {})
-        node = node_data[str(self.node_id)]
+        node = model_data[str(self.node_id)]
         self.next_nodes = node["nextNodes"]
         self.thresholds = sorted(node["thresholds"])
         return node
@@ -26,13 +22,21 @@ class Node:
 class Patient:
     """Patient class for navigating the decision tree"""
     def __init__(self):
-       self.current_node = Node("Start")   
+       self.data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir, "data/data.json"))
+       self.model_data = self.load_data()
+       self.current_node = Node("Start", model_data=self.model_data)   
        self.past_nodes = [] # list of nodes
        self.age = None
        self.blood_pressure = None
        self.pulse_ox = None
        self.glucose = None
-       self.current_node = None
+    
+    # loads data from json file
+    def load_data(self):
+        """loads data from json file"""
+        with open(self.data_path, 'r') as json_file:
+            model_data = json.load(json_file).get("Nodes", {})
+            return model_data
     
     # goes back to the last node visited
     def go_back(self):
@@ -45,7 +49,7 @@ class Patient:
         """""goes forward to the node with the given index in the nextNodes array"""""
         # If we know next node is last node, can't have its last node be this node
         self.past_nodes.append(self.current_node)
-        self.current_node = Node(node_id)
+        self.current_node = Node(node_id, self.model_data)
 
     # decides which node is next based on the input
     def decide(self, choice):
@@ -58,7 +62,7 @@ class Patient:
 
 
 p = Patient()
-print(p.current_node.id)
+print(p.current_node.node_id)
 p.decide(0)
-print(p.current_node.id)
+print(p.current_node.node_id)
 
