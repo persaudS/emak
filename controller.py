e88ad6b500e9dc03db46aca4c0fa3bef461fb96e
Siddharth Jan Simon <65785966+Siddysimon@users.noreply.github.com>
2023-09-21 01:09:29 +0000

    Breaking: new controller framework initial pass

--- controller.py
+++ controller.py
@@ -1,13 +1,52 @@
-from model import DecisionTree
-from devices import Device, BPCuff, PulseOx, Glucometer
+import logging
+from model import Patient
+from devices import BPCuff, PulseOx, Glucometer
 import view
 import devices
 
 
+class Controller:
+   """Controller class for connecting Model, View, and Devices"""
 
-def start():
-   view.startView()
+   def __init__(self):
+      self.patient = Patient()
+      self.view = View()
+      self.devices = []
+      self.devices.append(BPCuff())
+      self.devices.append(PulseOx())
+      self.devices.append(Glucometer())
+      self.logger = self._set_logger()
+      self._view_start()
+
+   def _set_logger(self):
+       logging.basicConfig(filename="EMAK.log",
+                           filemode='a',
+                           format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                           datefmt='%H:%M:%S',
+                           level=logging.DEBUG)
+
+       current_logger = logging.getLogger('EMAK')
+       return current_logger
+
+   def _view_start(self):
+      """Starts the view"""
+      states = {}
+      for device in self.devices:
+         states[device.name] = device.GetStatus()
+      logger.info("Starting GUI")
+      self.view.start(states)
+
+   def next():
+      # get input
+      choice = view.patientInput()
+      # decide next node
+      patient.decide(choice)
+
+      # display current node
+      view.display_node(patient.current_node)
+
 
 if __name__ == "__main__":
-   #running controller function
+   # running controller function
+   patient = Patient()
    start()
--- model/model.py
+++ model/model.py
@@ -23,7 +23,7 @@ class Patient:
     """Patient class for navigating the decision tree"""
     def __init__(self):
        self.data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir, "data/data.json"))
-       self.model_data = self.load_data()
+       self.model_data = self._load_data()
        self.current_node = Node("Start", model_data=self.model_data)   
        self.past_nodes = [] # list of nodes
        self.age = None
@@ -32,20 +32,20 @@ class Patient:
        self.glucose = None
     
     # loads data from json file
-    def load_data(self):
+    def _load_data(self):
         """loads data from json file"""
         with open(self.data_path, 'r') as json_file:
             model_data = json.load(json_file).get("Nodes", {})
             return model_data
     
     # goes back to the last node visited
-    def go_back(self):
+    def _go_back(self):
         """""goes back to the last node visited"""""
         if self.past_nodes:
             self.current_node = self.past_nodes.pop()
 
     # goes forward to the node with the given index in the nextNodes array
-    def go_forward(self, node_id):
+    def _go_forward(self, node_id):
         """""goes forward to the node with the given index in the nextNodes array"""""
         # If we know next node is last node, can't have its last node be this node
         self.past_nodes.append(self.current_node)
@@ -57,7 +57,7 @@ class Patient:
         i = 0
         for option in self.current_node.thresholds:
             if choice <= option:
-                self.go_forward(self.current_node.next_nodes[i])
+                self._go_forward(self.current_node.next_nodes[i])
             i += 1
 
 
