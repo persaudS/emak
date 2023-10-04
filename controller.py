a750fcec817c50d71f9ee4630d724adb51b3e058
seanpersaud <seanrp01@gmail.com>
2023-10-04 16:22:22 -0400

    Editing view.py interaction with controller.
    Co-authored-by: Siddharth Jan Simon <Siddysimon@users.noreply.github.com>

--- controller.py
+++ controller.py
@@ -5,62 +5,64 @@ from devices import BPCuff, PulseOx, Glucometer
 from view import MainView
 import devices
 
-#TODO: add observer pattern for devices
+# TODO: add observer pattern for devices
+
+
 class Controller:
-   """Controller class for connecting Model, View, and Devices"""
+    """Controller class for connecting Model, View, and Devices"""
 
-   def __init__(self):
-      self.view = MainView(self)
-      self.patient = Patient()
-      self.patient.add_observer(self) #Controller is an observer of Patient
-      self.add_observer(self.view) #View is an observer of Controller
-      self.add_observer(self.patient) #Patient is an observer of Controller
-      self.logger = self.set_logger()
-      self.logger.info("Starting GUI")
-      self.devices = []
-      self.devices.append(BPCuff())
-      self.devices.append(PulseOx())
-      self.devices.append(Glucometer())
-      self._observers = []
-      self.view_start()
+    def __init__(self):
+        self.view = MainView(self)
+        self.patient = Patient()
+        self.patient.add_observer(self)  # Controller is an observer of Patient
+        self.add_observer(self.view)  # View is an observer of Controller
+        self.add_observer(self.patient)  # Patient is an observer of Controller
+        self.logger = self.set_logger()
+        self.logger.info("Starting GUI")
+        self.devices = []
+        self.devices.append(BPCuff())
+        self.devices.append(PulseOx())
+        self.devices.append(Glucometer())
+        self._observers = []
+        self.view_start()
 
-   def set_logger(self):
-       logging.basicConfig(filename="EMAK.log",
-                           filemode='a',
-                           format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                           datefmt='%H:%M:%S',
-                           level=logging.DEBUG)
+    def set_logger(self):
+        logging.basicConfig(filename="EMAK.log",
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
 
-       current_logger = logging.getLogger('EMAK')
-       return current_logger
-   
-   # adds observer to list of observers
-   def add_observer(self, observer):
+        current_logger = logging.getLogger('EMAK')
+        return current_logger
+
+    # adds observer to list of observers
+    def add_observer(self, observer):
         self._observers.append(observer)
-   
-   def device_notify(self):
-       for observer in self._observers:
-           observer.update_metrics(self.devices)
-           
-   def view_start(self):
-      """Starts the view"""
-      states = {}
-      for device in self.devices:
-         states[device.name] = device.GetStatus()
-      self.logger.info("Adding device status to view")
-      self.view.set_states(states)
-      
-      self.view.start()
 
-   def next(self):#TODO
-      choice = self.view.patientInput()
-      # decide next node
-      self.patient.decide(choice)
+    def device_notify(self):
+        for observer in self._observers:
+            observer.update_metrics(self.devices)
 
-      # display current node
-      self.view.get_view_node(patient.current_node.node_id)
+    def view_start(self):
+        """Starts the view"""
+        states = {}
+        for device in self.devices:
+            states[device.name] = device.GetStatus()
+        self.logger.info("Adding device status to view")
+        self.view.set_states(states)
+
+        self.view.start()
+
+    def next(self):  # TODO
+        choice = self.view.patientInput()
+        # decide next node
+        self.patient.decide(choice)
+
+        # display current node
+        self.view.get_view_node(patient.current_node.node_id)
 
 
 if __name__ == "__main__":
-   # running controller function
-   c = Controller()
+    # running controller function
+    c = Controller()
--- view.py
+++ view.py
@@ -10,91 +10,123 @@ class MainView(tk.Tk):
 
     PAD = 10
 
-    def __init__(self, controller, nodeTitle="Start"):
+    def __init__(self, controller, nodeTitles=["Start"]):
         super().__init__()
-        self.data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir, "data/display.json"))
-        
+        self.data_path = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), os.pardir, "data/display.json"))
+
         self.controller = controller
-        
-        self.title(nodeTitle)
         self._make_main_frame()
-        
-        self.device_states = {} #TODO: get device states from controller
-        
-        ## generate frames
-        self.nodeFrame = NodeView(self, self._get_view_node(nodeTitle))
+
+        self.device_states = controller.devices.map(
+            lambda device: device.get_status())
+
+        # generate frames
+        self.nodeFrames = []
+        for nodeTitle in nodeTitles:
+            self.nodeFrames = self.nodeFrames.append(
+                NodeView(self, self._get_view_node(nodeTitle), self.controller, self))
+
         self.sensorFrame = SensorView(self, self.device_states)
-        
+
+        self.continueButton = ttk.Button(
+            self, text="Continue", command=self.on_state_change).pack(side="bottom")
+        self.continueButton["state"] = "disabled"
+        self.backButton = ttk.Button(
+            self, text="Back", command=self.on_state_change).pack(side="bottom")
+        self.endButton = ttk.Button(
+            self, text="EMS Arrived", command=self.on_state_change).pack(side="bottom")
 
     def start(self):
         self.mainloop()
-    
+
     def _make_main_frame(self):
         self.main_frame = ttk.Frame(self)
         self.main_frame.pack(padx=self.PAD, pady=self.PAD)
 
     def on_state_change(self):
-        #TODO: button was clicked
+        """Button submission event"""
+        if self.text == "Continue":
+            self.controller.next(self.nodeFrame.selected)
+        if self.text == "Back":
+            self.controller.next(0)
+        if self.text == "EMS Arrived":
+            self.controller.next(-1)
         pass
-    
+
     def set_states(self, states):
         """Sets the states of the devices"""
         self.device_states = states
-        #TODO: update view with new states
+        # TODO: update view with new states
         return None
-    
+
     def _get_view_node(self, nodeTitle):
         """Gets the node view information from json file"""
         with open(self.data_path, 'r') as json_file:
             view_data = json.load(json_file).get("Nodes", {})
             node = view_data.get(nodeTitle, {})
             return node
-        
+
     def update_metrics(self, devices):
         """Updates the view with new device states"""
         self.devices = devices
 
-class NodeView(tk.Frame):
-    def __init__(self, root, node):
+    def button_onclick(self, text):
+        if text == 'Back':
+            self.controller.back()
+        elif text == 'Continue':
+            # check if the Node views have selections
+            # if yes
+            self.controller.next()
+
+
+class NodeView():
+    def __init__(self, root, node, controller, main_view):
         tk.Frame(root, bg="red")
         self.text = node["text"]
         self.buttons = node["buttons"]
-        
+        self.controller = controller
+        self.selected = None
+        self.main_view = main_view
+
         self._make_text()
         self._make_buttons()
-        
-    
+
     def _make_text(self):
         """Creates prompt text for the node"""
         text_frame = ttk.Frame(self)
         text_frame.pack()
-        
+
         ttk.Label(text_frame, text=self.text).pack()
-    
+
     def _make_buttons(self):
         """Creates option buttons for the node"""
         button_frame = ttk.Frame(self)
         button_frame.pack()
-        
+
         for button in self.buttons:
-            ttk.Button(button_frame, text=button["text"], command=button_onclick(button["text"])).pack()
-    
-    def button_onclick(self, text):
+            ttk.Button(button_frame,
+                       text=button, command=self.button_onclick(button)).pack()
+
+    def button_onclick(self, button):
         """Button click event"""
-        pass
-    
+        for i in range(len(self.buttons)):
+            if self.buttons[i] == button:
+                break
+        self.selected = i
+        self.main_view.continueButton["state"] = "normal"
+        return
+
 
 class SensorView(tk.Frame):
     def __init__(self, root, devices):
         tk.Frame(root, bg="white")
         self.devices = devices
-    
+
     def update_metrics(self, devices):
         """Updates the view with new device states"""
         self.devices = devices
 
-        
-    
 
 if __name__ == "__main__":
     main = MainView("Start")
