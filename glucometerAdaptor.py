499dff7e10a6412b236619058c5f6b324c3c39b5
kcholden <kcholden@me.com>
2023-12-03 14:09:52 -0500

    gluc code

---
+++ glucometerAdaptor.py
@@ -0,0 +1,8 @@
+#this is what it should look like for getting the data from glucometer_adaptor
+
+'''
+def get_last_glucometer():
+    reading = glucometer_adapter.get_last()
+    print(reading)
+    return reading
+'''
